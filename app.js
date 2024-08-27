47f8d82abab6a1993268aede14bd2b56d2bdcbd2
itSarthak <sarthakt3006@gmail.com>
2024-08-27 13:38:18 +0530

    Removed Vercel

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ io.on("connection", (socket) => {
 });
 
 // 3) Listening on our server
-const PORT = process.env.PORT || 3040;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
